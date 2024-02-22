8c3e437184cadbc2167131635683bae9a308c1c8
Isabellylinda <isabelly.lbispo@gmail.com>
2024-02-22 11:44:20 -0400

    asd

---
+++ isalinda.js
@@ -0,0 +1,55 @@
+//atividade1
+var nome = "Isa linda";
+var idade = 15;
+console.log("meu nome é", nome, "e tenho", idade, "anos")
+
+
+//atividade2
+console.log('linda'.toUpperCase());
+console.log('linda'.toLowerCase());
+
+
+//atividade3
+var name3 = 'Isabelly'
+var nume2 = 99
+
+console.log(typeof name3)
+console.log(typeof nume2)
+
+
+//atividade4
+var imc= 52/(1.79*1.79);
+console.log(imc);
+
+
+//atividade5
+let num1 = 10;
+let num2 = 5;
+console.log("soma:",num1 + num2, "subtração:", num1 - num2, "multiplicacao:", num1*num2, "divisao:", num1/num2);
+
+
+//atividade6
+let idade2= 15
+let idade4= 17
+console.log(idade2>idade4);
+
+
+//atividade7
+var idade = 15;
+var conferir = idade >= 18 ? "Maior de idade" : "Menor de idade";
+console.log(conferir);
+
+
+//atividade8
+var numero6 = 35;
+var verificar= numero6 % 2 == 0? "o numero é par": "o numero é impar"
+console.log(verificar);
+
+
+
+
+
+
+
+
+
