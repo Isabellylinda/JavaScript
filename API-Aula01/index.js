16fd9464dc81f4750975224a9d000b68011ad82e
Isabellylinda <Isabelly.lbispo@gmail.com>
2024-08-13 10:23:34 -0400

    save01

--- API-Aula01/index.js
+++ API-Aula01/index.js
@@ -9,3 +9,7 @@ app.listen(8080, () => {
     let data = new Date();
     console.log("Servidor node iniciado em: " + data);
 })
+
+
+//sou aluno da noite, deixei salvo seu arquivo, voce deixou o computador ligado e desligou só o monitor.
+//se cuida isabelly linda
